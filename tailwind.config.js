701124bdf334015ab3cd53a113d298b4b63d2425
SanazBHMN <sanazbahmani35@gmail.com>
2022-02-20 11:50:04 +0330

    Define css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-primary": "var(--color-primary)",
+        "color-secondary": "var(--color-secondary)",
+        "text-primary": "var(--text-primary)",
+        "text-secondary": "var(--text-secondary)",
+      },
+    },
   },
   plugins: [],
 };
