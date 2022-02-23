949ee9e9d5981883a4c3a9710126491eacab97e9
SanazBHMN <sanazbahmani35@gmail.com>
2022-02-23 11:35:16 +0330

    Place the button in the right place

--- src/components/adviceCard/AdviceCard.js
+++ src/components/adviceCard/AdviceCard.js
@@ -1,12 +1,14 @@
 import { Advice } from "../advice/Advice";
 import { AdviceGenerator } from "../adviceGenerator/AdviceGenerator";
 import { AdviceId } from "../adviceId/AdviceId";
+import divider from "../../images/pattern-divider-mobile.svg";
 
 export const AdviceCard = () => {
   return (
     <div className="w-11/12 flex flex-col bg-color-secondary px-3 py-6 m-auto rounded-lg sm:w-96">
       <AdviceId />
       <Advice />
+      <img src={divider} />
       <AdviceGenerator />
     </div>
   );
--- src/components/adviceGenerator/AdviceGenerator.js
+++ src/components/adviceGenerator/AdviceGenerator.js
@@ -1,3 +1,11 @@
+import { DiceIcon } from "../../icons/DiceIcon";
+
 export const AdviceGenerator = ({ adviceId }) => {
-  return <button>AdviceGenerator</button>;
+  return (
+    <div className="flex justify-center align-middle -mb-14">
+      <button className="w-16 h-16 grid place-items-center  bg-text-secondary rounded-full">
+        <DiceIcon />
+      </button>
+    </div>
+  );
 };
