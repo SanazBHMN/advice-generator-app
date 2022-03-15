767be921f3d72627d9be9cf5609c3907a822d483
SanazBHMN <sanazbahmani35@gmail.com>
2022-03-15 13:12:22 +0330

    Change div tag in HorizontalLine to hr tag

--- src/components/line/HorizontalLine.js
+++ src/components/line/HorizontalLine.js
@@ -1,7 +1,7 @@
 export const HorizontalLine = ({ classes }) => {
   return (
-    <div
-      className={`absolute bottom-1 border border-b-1 border-text-primary ${classes}`}
-    ></div>
+    <hr
+      className={`absolute bottom-1 border-text-primary ${classes}`}
+    ></hr>
   );
 };
