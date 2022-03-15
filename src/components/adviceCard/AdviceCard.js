41084f568ff71727c504ee3d6bf8108c27844d22
SanazBHMN <sanazbahmani35@gmail.com>
2022-03-15 12:46:22 +0330

    Include Line in the design

--- src/components/adviceCard/AdviceCard.js
+++ src/components/adviceCard/AdviceCard.js
@@ -4,6 +4,7 @@ import { AdviceGenerator } from "../adviceGenerator/AdviceGenerator";
 import { AdviceId } from "../adviceId/AdviceId";
 import mobileDivider from "../../images/pattern-divider-mobile.svg";
 import desktopDivider from "../../images/pattern-divider-desktop.svg";
+import { HorizontalLine } from "../line/HorizontalLine";
 
 export const AdviceCard = () => {
   const [adviceId, setAdviceId] = useState("");
@@ -22,11 +23,15 @@ export const AdviceCard = () => {
     <div className="max-w-xs flex flex-col bg-color-secondary px-3 py-6 mx-auto mb-14 rounded-lg sm:max-w-md">
       <AdviceId adviceId={adviceId} />
       <Advice advice={advice} />
-      <div className="block sm:hidden">
+      <div className="block relative sm:hidden">
+        <HorizontalLine classes={"w-28 left-2"} />
         <img src={mobileDivider} />
+        <HorizontalLine classes={"w-28 right-2"} />
       </div>
-      <div className="hidden sm:block">
+      <div className="hidden relative sm:block">
+        <HorizontalLine classes={"w-40 left-2"} />
         <img src={desktopDivider} />
+        <HorizontalLine classes={"w-40 right-2"} />
       </div>
       <AdviceGenerator handleClick={handleClick} />
     </div>
