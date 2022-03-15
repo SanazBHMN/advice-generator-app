a14a98393cb320870f258c8261d9d8d1d0260782
SanazBHMN <sanazbahmani35@gmail.com>
2022-03-15 13:23:36 +0330

    Add my name

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { AdviceCard } from "./components/adviceCard/AdviceCard";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center h-screen">
-      <AdviceCard />
-    </div>
+    <>
+      <div className="flex flex-col justify-center h-screen">
+        <AdviceCard />
+      </div>
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/SanazBHMN">Sanaz Bahmani</a>.
+      </div>
+    </>
   );
 }
 
